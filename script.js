8f254d028cd52be37e2e727b4ec96a6d23058667
Atiq0629 <cross.016x@gmail.com>
2023-03-11 12:54:34 +0600

    First Commit

---
+++ script.js
@@ -0,0 +1,471 @@
+/* 
+let js = 'amazing'
+console.log(40+8+23-10);
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Bob";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";  
+
+let job1 = "programmer";
+let job2 = "teacher";  
+
+console.log(job2);
+*/
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof false);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+
+//  let catName = "Piku";
+
+//  console.log("My Cat's Name " + catName); 
+
+
+// let age= 30;
+// age= 31;
+
+
+// const birthYear = 1991;
+// birthYear = 1990;
+
+
+// let x = "john doe";
+// x = 0;
+
+// let x = 20 + 10; 
+// x += 100;
+// x *= 50;
+// x -= 500;
+// x ++;
+// x --;
+// x --;
+// console.log(x);
+
+
+
+// let y = 60;
+
+// console.log(x > y);
+// console.log(y >= 25);
+// console.log(y >=61);
+
+
+
+// let x= 100, y= 200;
+// if (x > y){
+//     console.log('hurrah');
+// }
+// else{
+//     console.log('gu');
+// }
+ 
+
+
+// let countryName = "Bangladesh";
+// let continentName = "Asia";
+// let population = "500 million";
+
+// console.log(countryName, continentName, population );
+
+
+//coding challaenge 01
+
+// let marksWeight = 78;
+// let johnsWeight = 92;
+
+// let marksHeight = 1.95;
+// let johnHeight = 1.76;
+
+// let marksBmi = marksWeight/(marksHeight * marksHeight);
+// //console.log(marksBmi);
+
+// let johnsBmi = johnsWeight/(johnHeight * johnHeight);
+// //console.log(johnsBmi);
+
+// if(marksBmi > johnsBmi){
+//     console.log('Marks BMI is higher than Johns. Which is ' + marksBmi );
+// }
+// else{
+//     console.log('Johns BMI is higher than Marks. Which is ' + johnsBmi );
+// }
+
+
+// const firstName = 'Piku';
+// const lastName = 'Billu';
+// const fullName = firstName + ' ' + lastName ;
+// const job = 'sleeping cat';
+// const birthYear = 2020;
+// const year = 2022;
+
+// const pikka = "I'm " + fullName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+// console.log(pikka);
+
+// const pikkaNew = `I'm ${fullName}, a ${year-birthYear} years old ${job} !`;
+// console.log(pikkaNew);
+
+
+// const marksMass = 78;
+// const marksHeight = 1.69;
+
+// const johnMass = 92;
+// const johnHeight = 1.95;
+
+// marksBmi = marksMass / (marksHeight * marksHeight);
+// johnBmi = johnMass/ (johnHeight * johnHeight);
+
+// console.log(marksBmi, johnBmi);
+
+// if(marksBmi> johnBmi){
+//     console.log('Marks Bmi is heigher than john. Which is ' + marksBmi);
+// }
+
+// else{
+//     console.log('John Bmi is heigher than Marks. Which is ' + johnBmi);
+// }
+
+// marksHigherBMI = marksBmi > johnBmi;
+
+// console.log(marksHigherBMI);
+
+// marksHigherBMI = marksBmi < johnBmi; 
+
+
+
+
+// const firstName = 'Tamanna';
+// const lastName = 'Amin';
+// const fullName = firstName + ' ' + lastName ;
+// const job ='Engineer';
+// const birthYear = 1994;
+// const Year = 2022;
+
+// const rumpa = `I"m ${fullName} , a ${Year-birthYear} years old ${job}`;
+// console.log(rumpa);
+
+
+// console.log(`string
+// multiple
+// line`);
+
+
+
+// let firstName = "Atiq";
+// let lastName = "Rahman";
+
+// let rubel = `Welcome ${firstName} ${lastName}`;
+// console.log(rubel);
+
+
+
+// const age = 15;
+
+// if (age >= 18){
+//     console.log('You can start driving now');
+// }
+// else{
+//     driveleft = 18-age;
+//     console.log(`Wait ${driveleft} years for your driving license`);
+// }
+
+
+
+// const birthYear = 1994;
+// let century;
+
+// if(birthYear <= 2022){
+//     century = 28;
+// }
+// else{
+//     century = 29;
+// }
+ 
+// console.log(century);
+
+
+
+//if-else code1
+
+// const age = 13;
+// const ownsCar = true;
+
+// if(age >= 16 && ownsCar){
+//     console.log("Jerry is old enough to drive and has his own car.");
+// }
+// else{
+//     console.log("Jerry does not drive.");
+// }
+
+
+
+
+//if-else code2
+
+// const age = 15;
+
+// if(age >= 18){
+//     console.log("Nick is an adult");
+// }
+// else{
+//     console.log("Nick is a child");
+// }
+
+
+
+//if-else code3
+
+// const age = 18;
+
+// if(age < 18){
+//     console.log("Alice is under 18years old");
+// }
+// else if (age >= 18 && age <=21){
+//     console.log("Alice is between the ages of 18 and 21");
+// }
+
+// else{
+//     console.log("Alice is over 21 years old");
+// }
+
+
+//if-else code4
+
+// let atiqSalary = 500;
+// let rumpaSalary = 1000;
+
+// if(atiqSalary > rumpaSalary){
+//     console.log("Atiq salary is greater than rumpa slary");
+// }
+// else{
+//     console.log("Atiq salary is less than or equal to rumpa salary");
+// }
+
+
+
+
+//if-else code5
+
+// const age = 32; 
+// let citizen;
+
+// if (age >= 18){
+//     citizen = "can vote";
+// }
+
+// else{
+//     citizen = "cannot vote";
+// }
+
+// console.log(citizen);
+
+
+
+
+
+// const inputYear = '1991';
+// console.log(Number(inputYear));
+// console.log(Number(inputYear) +18);
+
+
+
+// const inputYear = '2009';
+// console.log(Number(inputYear)+200);
+
+
+// const age  = 18
+
+// console.log(age == '20'-2);
+
+
+
+
+// const favourite = Number (prompt("What's your favourite number?"));
+// console.log(favourite);
+// console.log(typeof favourite);
+
+
+// if (favourite === 23) {
+//     console.log ('cool! 23 is an amazing number!');
+// }
+
+// else if (favourite === 7) {
+//     console.log('7 is also a cool number');
+// }
+// else if (favourite === 9) {
+//     console.log('9 is also a cool number');
+// }
+// else if (favourite !== 23 || favourite !== 7 || favourite !== 9 ){
+//     console.log('input number is ' + favourite);
+// }
+
+
+//     else {
+//         console.log ('Number is not 23 or 7 or 9');
+//     }
+
+
+
+
+// const inputYear = '1990';
+// console.log(Number(inputYear));
+// console.log(Number(inputYear)+18);
+
+
+
+// const hasDriverLicense = true ;
+// const hasGoodVision = true ;
+
+
+
+// const isTired = false;
+// console.log(hasDriverLicense && hasGoodVision && isTired); 
+
+
+// if (hasDriverLicense && hasGoodVision && !isTired){
+//     console.log('sarah is able to drive');
+// }
+   
+// else{
+//     console.log("someone else should drive");
+// }
+
+
+
+
+// let dolphinsScore1 = 96;
+// let dolphinsScore2 = 108;
+// let dolphinsScore3 = 89;
+
+// dolphinAverScore = (dolphinsScore1 + dolphinsScore2 + dolphinsScore3)/3 ;
+
+// console.log (dolphinAverScore);
+
+
+// let koalasScore1 = 88;
+// let koalasScore2 = 91;
+// let koalasScore3 = 110;
+
+// koalaAverScore = (koalasScore1 + koalasScore2 + koalasScore3)/3 ;
+
+// console.log (koalaAverScore);
+
+
+// if (dolphinAverScore > koalaAverScore && dolphinAverScore >= 100){
+//     console.log ('Dolphin is win');
+// }
+
+// else if(koalaAverScore > dolphinAverScore && koalaAverScore >= 100){
+//     console.log ('Koala is win');
+// }
+
+// else if(dolphinAverScore === koalaAverScore && dolphinAverScore >= 100 && koalaAverScore >= 100 )
+//     {
+//         console.log('draw');
+//     }
+// else{
+//     console.log ('no team wins the trophy')
+// }
+
+// const day ='sunday'; 
+
+// switch(day){
+//     case 'monday':
+//     console.log('plan course structure');
+//     console.log('go to coding');
+//     break;  
+
+//     case 'thuesday':
+//     console.log('prepare theory videos');
+//     break; 
+      
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log('write code examples');
+//         break;
+
+//         case 'friday':
+//             console.log('Record videos');
+//             break;
+
+//             case 'saturday':
+//                 case'sunday':
+//                 console.log('enjoy the weekend');
+//                 break;
+//                 default:
+//                     console.log('Not a valid day!');
+
+
+
+
+// const bill = 375;
+// let tip;
+
+
+
+// if(bill>50 && bill<300){
+//   tip = (bill * .15 );
+// }
+// else if(bill>300){
+//      tip = (bill * .20);
+// }
+
+
+// console.log(tip);
+
+// let total = bill + tip ;
+// console.log (total);
+
+
+
+// const bill = 400;
+// let tip;
+//  bill>50 && bill<300 ? console.log(tip = bill * .15) : console.log(tip = bill * .20);
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
